2795e814d14fdb7e7a888e830ab83e7f199d1367
santfernandez <you@example.com>
2022-06-25 19:53:39 -0300

    changed rsvp button style

--- src/sections/RSVP.js
+++ src/sections/RSVP.js
@@ -1,18 +1,22 @@
 import React, { useState } from "react";
 import "../styles/RSVP.css";
-import RSVPModal from "../components/RSVPModal";
 
 const RSVP = () => {
-  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="rsvp-container">
       <div className="rsvp">
         <h2 className="rsvp-title">Asistencia</h2>
         <p className="rsvp-info">Confirmar antes del 30 julio</p>
-        <button type="button" onClick={() => setIsOpen(true)}>
-          Confirmar
+        <button type="rsvp-button">
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://docs.google.com/forms/d/e/1FAIpQLScCKG7CQCAkjVcTWvj0w_xnPICWaQZKLGdgM26OSJwOGzXx8A/viewform"
+            className="rsvp-link"
+          >
+            Confirmar
+          </a>
         </button>
-        <RSVPModal open={isOpen} onClose={() => setIsOpen(false)}></RSVPModal>
       </div>
     </div>
   );
